931946bcf4fb82995d5901b1aa44c7a44f470ea6
Laboratorio de Software <lds@unac.edu.co>
2022-03-08 19:32:39 -0500

    Initial commit

---
+++ src/main/java/com/example/Vitarico/VitaricoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vitarico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaricoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Vitarico/VitaricoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vitarico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaricoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
